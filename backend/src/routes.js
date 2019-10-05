1b155ac5fd5bec399d042be98db514d24e2aaabb
DavyMassoneto <davy.massoneto@yahoo.com>
2019-10-05 10:57:13 -0300

    Creating a listing for spots
    Signed-off-by: DavyMassoneto <davy.massoneto@yahoo.com>

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -11,6 +11,7 @@ const upload = multer(uploadConfig)
 
 routes.post('/sessions', SessionController.store)
 
+routes.get('/spots', SpotController.index)
 routes.post('/spots', upload.single('thumbnail'), SpotController.store)
 
 module.exports = routes;
