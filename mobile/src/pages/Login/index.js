fdf55d3a35a4a1d8fa9079fcc770efe07f1a1d0f
DavyMassoneto <davy.massoneto@yahoo.com>
2019-11-29 00:03:13 -0300

    implementing login function
    Signed-off-by: DavyMassoneto <davy.massoneto@yahoo.com>

--- mobile/src/pages/Login/index.js
+++ mobile/src/pages/Login/index.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { AsyncStorage } from 'react-native'
 
-import { Button, ButtonText, Container, Form, Input, Label, Logo } from "./styles";
+import api from '../../services/api'
 
-export default function Login() {
+import { Button, ButtonText, Container, Form, Input, Label, Logo } from './styles'
+
+export default function Login ({ navigation }) {
+  const [ email, setEmail ] = useState()
+  const [ techs, setTechs ] = useState()
+
+  useEffect(onLoad, [])
+
+  function onLoad () {
+    checkAlreadyLogged()
+  }
+
+  async function checkAlreadyLogged () {
+    const user = await AsyncStorage.getItem('user')
+    if (user) {
+      navigation.navigate('List')
+    }
+  }
+
+  async function handleSubmit () {
+    const response = await api.post('/sessions', { email })
+
+    if (response.status === 200) {
+      const { _id } = response.data
+
+      await AsyncStorage.setItem('user', _id)
+      await AsyncStorage.setItem('techs', techs)
+
+      navigation.navigate('List')
+    }
+  }
+
   return (
     <Container>
       <Logo source={require('../../assets/logo.png')} />
@@ -11,12 +43,16 @@ export default function Login() {
         <Input placeholder='Seu e-mail'
                keyboardType='email-address'
                autoCapitalize='none'
-               autoCorrect={false} />
+               autoCorrect={false}
+               value={email}
+               onChangeText={setEmail} />
         <Label>TECNOLOGIAS *</Label>
         <Input placeholder='Tecnologias de interesse'
                autoCapitalize='words'
-               autoCorrect={false} />
-        <Button>
+               autoCorrect={false}
+               value={techs}
+               onChangeText={setTechs} />
+        <Button onPress={handleSubmit}>
           <ButtonText>Encontrar spots</ButtonText>
         </Button>
       </Form>
